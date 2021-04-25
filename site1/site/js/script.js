630b19015e5d258938e91f1033fedf168ad97482
Maaaaxim <maxim.kubichka@gmail.com>
2021-04-25 22:22:58 +0300

    proverka_bootsrapa

--- site1/site/js/script.js
+++ site1/site/js/script.js
@@ -1,11 +1,9 @@
-$(document).ready(function () {
-	$('.header__burger').click(function (event) {
-		$('.header__burger,.header__menu,.header__link').toggleClass('active');
-		$('body').toggleClass('lock');
-	});
-});
-alert('Вы были замечены за дрочкой, видеозапись Вашего рукоблудствабыла сохранена. Чтобы видео было удалено, введите данные своей банковсокй карты в слудующих всплывающих окнах')
-prompt('номер карты')
-prompt('дата на карте')
-prompt('три цифры сзади')
-alert('дрочите дальше))')
+
+function sayHello() {
+  var say = function() { console.log(hello); }
+  // Local variable that ends up within the closure 
+  var hello = 'Hello, world!';
+  return say;
+}
+var sayHelloClosure = sayHello(); 
+sayHelloClosure(); // ‘Hello, world!’
